498438b33b2f54202188b75b37d147a554d24d89
Hazrian <chazotflorian@gmail.com>
2020-11-20 11:34:10 +0100

    [add] Specialite Service

---
+++ src/app/service/specialite/specialite.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Specialite } from './../../model/Specialite';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpecialiteService {
+  URL = 'http://localhost:'; // TODO mettre la bonne url
+  specialite0 : Specialite = {
+    id:0,
+    nomSpe:"medecin traitant"
+  }
+  specialite1 : Specialite = {
+    id:1,
+    nomSpe:"dentiste"
+  }
+  specialite2 : Specialite = {
+    id:2,
+    nomSpe:"Orthophoniste"
+  }
+  specialite3 : Specialite = {
+    id:3,
+    nomSpe:"chirurgien"
+  }
+  listeSpecialite : Specialite[] = [this.specialite0,this.specialite1,this.specialite2,this.specialite3];
+
+constructor(private http: HttpClient) { }
+
+// Récupère une specialite avec son id 
+  // TODO remplacer par l'appel au back
+  getPatient(id:number){
+    return this.listeSpecialite[id]
+  }
+  
+  // Récupère la liste des specialites  
+  // TODO remplacer par l'appel au back
+  getPatients(){
+    return this.listeSpecialite
+  }
+
+   // rajouter les autres appel au back
+}
+
+}
