0c31baea1772affda999ef0703b6c61fa200c296
damscheb@gmail.com <damscheb@gmail.com>
2020-11-25 16:48:53 +0100

    [add] fonctionnalite medecin

--- src/app/2_Medecin/medecin-routing.module.ts
+++ src/app/2_Medecin/medecin-routing.module.ts
@@ -1,3 +1,5 @@
+import { FichesMedecinComponent } from './fiches-medecin/fiches-medecin.component';
+import { PatientsMedecinComponent } from './patients-medecin/patients-medecin.component';
 import { UpdateMedecinComponent } from './update-medecin/update-medecin.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -6,7 +8,9 @@ import { AccueilMedecinComponent } from './accueil-medecin/accueil-medecin.compo
 const medecinRoutes: Routes = [
 
   { path: '', component: AccueilMedecinComponent  },
-  { path: 'update', component: UpdateMedecinComponent }
+  { path: 'update', component: UpdateMedecinComponent },
+  { path: 'patients', component: PatientsMedecinComponent },
+  { path: 'fiches', component: FichesMedecinComponent }
 ];
 
 @NgModule({
