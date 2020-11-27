ac66168bdb50082b1e328926c21ba31fbcc3eb40
damscheb@gmail.com <damscheb@gmail.com>
2020-11-27 10:52:30 +0100

    [update] infoConsult

--- src/app/1_Patient/infoConsultation-patient/infoConsultation-patient.component.ts
+++ src/app/1_Patient/infoConsultation-patient/infoConsultation-patient.component.ts
@@ -1,6 +1,11 @@
+import { Specialite } from './../../model/Specialite';
+import { Adresse } from './../../model/Adresse';
+import { MedecinService } from './../../service/medecin/medecin.service';
+import { ConsultationService } from './../../service/consultation/consultation.service';
 import { Consultation } from './../../model/Consultation';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Medecin } from '../../model/Medecin';
+import { ModalDirective } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-infoConsultation-patient',
@@ -8,10 +13,38 @@ import { Medecin } from '../../model/Medecin';
   styleUrls: ['./infoConsultation-patient.component.scss']
 })
 export class InfoConsultationPatientComponent implements OnInit {
+  @ViewChild('infoConsultation') public infoConsultation: ModalDirective;
+  @Input()
+  infoId: number;
+  id: number;
+  consultation: Consultation = new Consultation();
+  medecin: Medecin = new Medecin();
+  adresse: Adresse = new Adresse();
+  specialite: Specialite = new Specialite();
 
-  constructor() { }
+  constructor(
+    private consultationService: ConsultationService,
+    private medecinService: MedecinService
+  ) { }
 
   ngOnInit() {
+    this.medecin.adresse = this.adresse;
+    this.medecin.specialite = this.specialite;
+  }
+
+  showModalInfoConsultation(id: number) {
+    this.id = id;
+    this.consultationService.getConsultation(this.id).subscribe(
+      (response: any) => {
+        this.consultation = response.data;
+        this.medecinService.getMedecin(this.consultation.medecin.id).subscribe(
+          (response: any) => {
+            this.medecin = response.data;
+          }
+        )
+      }
+    )
+    this.infoConsultation.show();
   }
 
 }
